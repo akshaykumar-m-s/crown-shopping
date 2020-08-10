b8e007f77e2bde4e07efa07d81cefb3d1f411cff
Akshay Kumar M S <akshay.k@thinkpalm.com>
2020-08-11 02:07:21 +0530

    FormInput & CustomButton created

---
+++ src/pages/sign-in-and-sign-up/sign-in-and-sign-up.component.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import './sign-in-and-sign-up.component.scss';
+import { SignIn } from '../../components/sign-in/sign-in.component';
+
+export const SignInAndSignUpPage = () => (
+    <div className='sign-in-and-sign-up'>
+        <SignIn />
+    </div>
+);
+

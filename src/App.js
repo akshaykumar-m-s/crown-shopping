1d363ba1a6e2535499a3363a0847aa6ca6303946
Akshay Kumar M S <akshay.k@thinkpalm.com>
2020-08-11 00:22:43 +0530

    Collection preview donw

--- src/App.js
+++ src/App.js
@@ -2,19 +2,14 @@ import React from 'react';
 import './App.css';
 import HomePage from './pages/homepage/hompage.component';
 import { Switch, Route } from 'react-router-dom';
-
-const HatPage = () => (
-  <div>
-    <h1>HATS PAGE</h1>
-  </div>
-);
+import ShopPage from './pages/shop/shop.component';
 
 function App() {
   return (
     <div>
       <Switch>
         <Route exact path='/' component={HomePage} />
-        <Route path='/hats' component={HatPage} />
+        <Route path='/shop' component={ShopPage} />
       </Switch>
     </div>
   );

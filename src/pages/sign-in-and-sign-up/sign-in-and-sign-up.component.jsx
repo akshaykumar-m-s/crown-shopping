9dfc1d7ebfd2513b5697d8c18bc9087a62645c46
Akshay Kumar M S <akshay.k@thinkpalm.com>
2020-08-12 00:53:48 +0530

    store email-pwd to firestore

--- src/pages/sign-in-and-sign-up/sign-in-and-sign-up.component.jsx
+++ src/pages/sign-in-and-sign-up/sign-in-and-sign-up.component.jsx
@@ -1,10 +1,11 @@
-import React from 'react';
-import './sign-in-and-sign-up.component.scss';
-import { SignIn } from '../../components/sign-in/sign-in.component';
+import React from "react";
+import "./sign-in-and-sign-up.component.scss";
+import { SignIn } from "../../components/sign-in/sign-in.component";
+import { SignUp } from "../../components/sign-up/sign-up.component";
 
 export const SignInAndSignUpPage = () => (
-    <div className='sign-in-and-sign-up'>
-        <SignIn />
-    </div>
+  <div className="sign-in-and-sign-up">
+    <SignIn />
+    <SignUp />
+  </div>
 );
-

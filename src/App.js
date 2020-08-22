c965f1309ba6bc1967e99c641446ec514c4e8f5f
Akshay Kumar M S <akshay.k@thinkpalm.com>
2020-08-22 23:56:55 +0530

    removed code for bulk upload collection

--- src/App.js
+++ src/App.js
@@ -4,24 +4,19 @@ import HomePage from './pages/homepage/homepage.component';
 import { Switch, Route, Redirect } from 'react-router-dom';
 import ShopPage from './pages/shop/shop.component';
 import Header from './components/header/header.component';
-import {
-  auth,
-  createUserProfileDocument,
-  addCollectionAndDocuments,
-} from './firebase/firebase.utils';
+import { auth, createUserProfileDocument } from './firebase/firebase.utils';
 import { connect } from 'react-redux';
 import { setCurrentUser } from './redux/user/user.actions';
 import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
 import { createStructuredSelector } from 'reselect';
 import { selectCurrentUser } from './redux/user/user.selector';
 import CheckoutPage from './pages/checkout/checkout.component';
-import { selectCollectionsForPreview } from './redux/shop/shop.selector';
 
 class App extends Component {
   unsubscribeFromAuth = null;
 
   componentDidMount() {
-    const { setCurrentUser, collectionsArray } = this.props;
+    const { setCurrentUser } = this.props;
 
     this.unsubscribeFromAuth = auth.onAuthStateChanged(async (userAuth) => {
       if (userAuth) {
@@ -36,10 +31,6 @@ class App extends Component {
       }
 
       setCurrentUser(userAuth);
-      addCollectionAndDocuments(
-        'collections',
-        collectionsArray.map(({ title, items }) => ({ title, items }))
-      );
     });
   }
 
@@ -74,7 +65,6 @@ class App extends Component {
 
 const mapStateToProps = createStructuredSelector({
   currentUser: selectCurrentUser,
-  collectionsArray: selectCollectionsForPreview,
 });
 
 const mapDispatchToProps = (dispatch) => ({
--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -40,21 +40,21 @@ export const createUserProfileDocument = async (userAuth, additionalData) => {
   return userRef;
 };
 
-export const addCollectionAndDocuments = async (
-  collectionKey,
-  objectsToAdd
-) => {
-  const collectionRef = firestore.collection(collectionKey);
-  console.log('collections', collectionRef);
+// export const addCollectionAndDocuments = async (
+//   collectionKey,
+//   objectsToAdd
+// ) => {
+//   const collectionRef = firestore.collection(collectionKey);
+//   console.log('collections', collectionRef);
 
-  const batch = firestore.batch();
-  objectsToAdd.forEach((element) => {
-    const newDocRef = collectionRef.doc();
-    batch.set(newDocRef, element);
-  });
+//   const batch = firestore.batch();
+//   objectsToAdd.forEach((element) => {
+//     const newDocRef = collectionRef.doc();
+//     batch.set(newDocRef, element);
+//   });
 
-  return await batch.commit();
-};
+//   return await batch.commit();
+// };
 
 export const auth = firebase.auth();
 export const firestore = firebase.firestore();

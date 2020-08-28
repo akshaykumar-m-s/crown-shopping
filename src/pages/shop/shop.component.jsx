04a67d827c2bd3f1d0efa4d760552adcccd7a8f6
Akshay Kumar M S <akshay.k@thinkpalm.com>
2020-08-29 01:23:51 +0530

    App.js and Shop component updated with useState hook

--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { useEffect } from 'react';
 import './shop.component.scss';
 import CollectionOverviewContainer from '../../components/collection-overview/collection-overview.container';
 import { Route } from 'react-router-dom';
@@ -6,29 +6,25 @@ import { fetchCollectionsStart } from '../../redux/shop/shop.actions';
 import { connect } from 'react-redux';
 import CollectionPageContainer from '../collection/collection.container';
 
-class ShopPage extends Component {
-  componentDidMount() {
-    const { fetchCollectionsStart } = this.props;
+const ShopPage = ({ fetchCollectionsStart, match }) => {
+  useEffect(() => {
     fetchCollectionsStart();
-  }
+  }, [fetchCollectionsStart]);
 
-  render() {
-    const { match } = this.props;
-    return (
-      <div className="shop-page">
-        <Route
-          exact
-          path={`${match.path}`}
-          component={CollectionOverviewContainer}
-        />
-        <Route
-          path={`${match.path}/:collectionId`}
-          component={CollectionPageContainer}
-        />
-      </div>
-    );
-  }
-}
+  return (
+    <div className="shop-page">
+      <Route
+        exact
+        path={`${match.path}`}
+        component={CollectionOverviewContainer}
+      />
+      <Route
+        path={`${match.path}/:collectionId`}
+        component={CollectionPageContainer}
+      />
+    </div>
+  );
+};
 
 const mapDispatchToProps = (dispatch) => ({
   fetchCollectionsStart: () => dispatch(fetchCollectionsStart()),
